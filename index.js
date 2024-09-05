1ad496c5de116651bfbe7e55fd0bba142aed37d2
GustavoMachadolindaoo <127844882+Gustavo0Machado@users.noreply.github.com>
2024-09-05 01:06:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const introducaoum = "olá, tudo bem?";
+const introducaodois = "Deixe-me apresentar:";
+const primeiroNome = "Gustavo";
+const sobreNome = "Machado";
+const idade  = 45;
+const altura  = 1.73;
+const cidadeNatal  = "Curitiba";
+const massa  = "75kg";
+
+
+console.log(introducaoum);
+console.log(introducaodois);
+console.log(primeiroNome);
+console.log(sobreNome);
+console.log(idade) ;
+console.log(altura);
+console.log(cidadeNatal);
+console.log(massa);
